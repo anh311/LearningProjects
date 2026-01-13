19a8d6491483d772acebf1aef90f0894d48d89af
tl3672s <anh.le@alumni.fh-aachen.de>
2026-01-13 02:30:50 +0100

    48

---
+++ Leetcode Challenges/Medium/Math/(48)RotateImage.c
@@ -0,0 +1,25 @@
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    int n= matrixSize;
+    for (int layer = 0; layer < n / 2; layer++) {
+        int first = layer;
+        int last = n - 1 - layer;
+        for (int i = first; i < last; i++) {
+            int offset = i - first;
+            // speichere top
+            int top = matrix[first][i];
+
+            // left -> top
+            matrix[first][i] = matrix[last - offset][first];
+
+            // bottom -> left
+            matrix[last - offset][first] = matrix[last][last - offset];
+
+            // right -> bottom
+            matrix[last][last - offset] = matrix[i][last];
+
+            // top -> right
+            matrix[i][last] = top;
+        }
+    }
+}
+
