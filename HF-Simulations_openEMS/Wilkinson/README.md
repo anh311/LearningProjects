535cecdcea58a75c7f35aad369bb5f6e17cc5e40
tl3672s <anh.le@alumni.fh-aachen.de>
2026-01-22 22:21:40 +0100

    Update

---
+++ HF-Simulations_openEMS/Wilkinson/README.md
@@ -0,0 +1,61 @@
+# Wilkinson Power Divider with Microstrip (FAILED – my structure was bad and SMD resistor needed)
+
+## Goal
+- Try simulate a Wilkinson Power Divider at 1 GHz
+- Check how it splits power and matches the ports
+
+
+## Setup
+- FR4 board with single-sided copper (1.5 mm thick)
+- 50 Ω main microstrip line (3 mm wide)
+- λ/4 microstrip section: 3.7 mm long, 1.5 mm wide (~70 Ω)
+- 100 Ω resistor
+
+## Theory
+- A Wilkinson Power Divider splits input power evenly between two outputs (~-3 dB each)
+- All ports are ideally matched, and the divider is theoretically lossless
+- Isolation resistor ensures that the outputs don’t interfere with each other
+- The λ/4 lines transform impedances to make everything match nicely
+
+    <img src="img/Wilkinson_Power_Divider.png" alt=" Wilkinson Power Divider" width="500"/>
+
+## Simulations
+
+- **Setup:**
+  
+    <img src="img/Wilkinson.png" alt=" Wilkinson Power Divider" width="500"/>
+   
+- **Action:**
+    simulate S21,S31,S11
+  
+    Port 2
+  
+    <img src="img/S21_S11.png.png" alt=" Wilkinson Power Divider" width="500"/>
+    
+    Port 3
+  
+    <img src="img/S31_S11.png.png" alt=" Wilkinson Power Divider" width="500"/>
+
+    simulate S12,S32,S22
+  
+    Port 1
+  
+    <img src="img/S12_S22.png.png" alt=" Wilkinson Power Divider" width="500"/>
+    
+    Port 3
+  
+    <img src="img/S31_S11.png.png" alt=" Wilkinson Power Divider" width="500"/>
+
+    simulate S13,S23,S33
+  
+    Port 1
+  
+    <img src="img/S13_S33.png.png" alt=" Wilkinson Power Divider" width="500"/>
+    
+    Port 3
+  
+    <img src="img/S23_S33.png.png" alt=" Wilkinson Power Divider" width="500"/>
+     
+    
+- **Observation / Note:** 
+    
---
+++ HF-Simulations_openEMS/Wilkinson/WilkinsonPowerDividers.m
@@ -0,0 +1,88 @@
+
+% WilkinsonPowerDividers
+close all
+clear
+clc
+%add path for openEMS
+addpath('C:\openEMS\matlab');
+
+c0=3e8;
+f_max=3e9;
+unit = 1e-3;
+resolution = c0/(f_max*sqrt(4.2))/unit/60;
+
+%init CSX Datastructure for openEMS
+CSX = InitCSX();
+
+%define materials8
+CSX = AddMaterial(CSX,"FR4");
+CSX = SetMaterialProperty(CSX, "FR4", "Epsilon", 4.2);
+CSX = AddMetal(CSX,"Copper")
+
+%define geometry (make the transmissionline shorter for faster Simulation)
+CSX = AddBox(CSX,"FR4",0,[-50, -30, 0],[50, 30, 1.5]);         %FR4
+CSX = AddBox(CSX,"Copper",1,[-50, -30, 0],[50, 30, 0]);  %GND
+CSX = AddBox(CSX,"Copper", 1,[-50, 10, 1.5],[-12, 13, 1.5]); %Line
+CSX = AddBox(CSX,"Copper", 1,[1.5-15,3.4,1.5], [3-15,10, 1.5]);
+CSX = AddBox(CSX,"Copper", 1,[1.5-15,13,1.5], [3-15,19.6, 1.5]);
+CSX = AddBox(CSX,"Copper", 1,[1.5-15, 3.4, 1.5],[31.5-15,1.9 , 1.5]);
+CSX = AddBox(CSX,"Copper", 1,[1.5-15, 19.6, 1.5],[31.5-15,21.1 , 1.5]);
+CSX = AddBox(CSX,"Copper", 1,[31.5-15, 3.4+0.75, 1.5],[50,1.9-0.75 , 1.5]);
+CSX = AddBox(CSX,"Copper", 1,[31.5-15, 19.6-0.75, 1.5],[50,21.1+0.75 , 1.5]);
+
+CSX = AddLumpedElement( CSX, 'Resistor', 1, 'Caps', 1, 'R', 100 );
+CSX = AddBox( CSX, 'Resistor', 1, [31.5-15 3.4+0.75 1.5], [31.5-12 19.6-0.75 1.5] );
+%define mesh
+mesh.x = SmoothMeshLines([-60 -50 0 50 60], resolution, 1.3);
+mesh.y = SmoothMeshLines([-40 -30 10 13 30 40 ], resolution, 1.3);
+mesh.z = SmoothMeshLines([-5 -1.5 0 1.5 10], resolution, 1.5);
+
+
+%define Grid
+%mash in mm (FDTD)
+CSX = DefineRectGrid( CSX, 1e-3, mesh );
+
+%define Ports
+[CSX,port{1}] = AddMSLPort( CSX, 9, 1, 'Copper', [-50 10 1.5], [-20 13 0], 0, [0 0 -1], 'ExcitePort',true,'FeedShift', 10*resolution, "Feed_R",50);
+[CSX,port{2}] = AddMSLPort( CSX, 9, 2, 'Copper', [50 3.4+0.75 1.5], [30 1.9-0.75 0], 0, [0 0 -1],'ExcitePort', false, "Feed_R",50);
+[CSX,port{3}] = AddMSLPort( CSX, 9, 3, 'Copper', [50 19.6-0.75 1.5], [30 21.1+0.75 0], 0, [0 0 -1],'ExcitePort', false,"Feed_R",50);
+
+
+%FDTD 0 GHz - 3 GHz
+##F0 = 1.75e9;
+##Fc = 1.25e9;
+F0 = 3e9;
+Fc = 3e9;
+FDTD = InitFDTD("NRTS",3e5, "EndCriteria",1e-4);
+FDTD = SetGaussExcite(FDTD,F0,Fc);
+FDTD = SetBoundaryCond(FDTD, {'PML_8' 'PML_8' 'MUR' 'MUR' 'PEC' 'MUR'})
+
+
+%Save Data
+WriteOpenEMS("temp/WilkinsonPowerDividers.xml",FDTD, CSX);
+
+%model Display
+CSXGeomPlot("temp/WilkinsonPowerDividers.xml");
+
+%run simulation
+RunOpenEMS( "temp", "WilkinsonPowerDividers.xml" );
+
+% Display simulation
+close all
+f = linspace( 0.5e9, 3e9, 400);
+
+port = calcPort( port, "temp", f, 'RefImpedance', 50);
+s11 = port{1}.uf.ref./ port{1}.uf.inc;
+s21 = port{2}.uf.ref./ port{1}.uf.inc;
+
+plot(f/1e9,20*log10(abs(s11)),'k-','LineWidth',1);
+hold on;
+grid on;
+grid minor;
+plot(f/1e9,20*log10(abs(s21)),'r--','LineWidth',1);
+title('Microstrip S-Parameters Simulation', 'FontSize', 18);
+hLeg = legend('S_{11}', 'S_{21}','FontSize', 12);
+set(hLeg, 'Box', 'off', 'Location', 'northeastoutside');
+ylabel('S-Parameter (dB)','FontSize',16);
+xlabel('frequency (GHz) \rightarrow','FontSize',16);
+ylim([-80 2]);
