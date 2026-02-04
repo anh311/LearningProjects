983058bfd5615629d256cf56a0cd8abd44531f6e
tl3672s <anh.le@alumni.fh-aachen.de>
2026-02-04 12:55:13 +0100

    Update

---
+++ LeetcodeChallenges/Easy/Bit Manipulation/(190)ReverseBits.c
@@ -0,0 +1,11 @@
+int reverseBits(int n) {
+    int result=0;
+    for (int i = 0; i < 32; i++) {
+        result <<= 1;
+        if (n & 1) {
+            result |= 1;
+        }
+        n >>= 1;
+    }
+    return result;
+}
