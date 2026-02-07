d0c51de54f3b25cb2b419ea3ad460dddd6105b2f
tl3672s <anh.le@alumni.fh-aachen.de>
2026-02-07 22:59:28 +0100

    Update

---
+++ LeetcodeChallenges/Easy/Bit Manipulation/(2980)CheckIfBitwiseOrHasTrailingZeros.c
@@ -0,0 +1,8 @@
+bool hasTrailingZeros(int* nums, int numsSize) {
+    int count=0;
+    for(int i=0;i<numsSize;i++){
+        if((nums[i]&1)!=1) count+=1;
+        if(count>=2)return true;
+    }
+    return false;
+}
